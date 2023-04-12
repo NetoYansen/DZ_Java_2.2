2f3ae24890814e6c4ea8762273083be5b9bd626a
User <yansenlv@mail.ru>
2023-04-12 22:49:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+
+        int start = 100;
+        int amount = 1350;
+        int bonus = amount / 100;
+        if (amount > 1000) {
+            System.out.println("Бонус за оплату:" + amount / 100);
+        } else {
+            System.out.println("Бонус за оплату:0");
+        }
+        if (amount <= 1000) {
+            System.out.println("На счете:" + (start + amount));
+        } else {
+            System.out.println("На счете:" + (start + amount + bonus));
+        }
+    }
+}
